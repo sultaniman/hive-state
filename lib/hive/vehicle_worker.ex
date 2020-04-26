4a0649c1c6b6ef15e5650733769e2a6d7e8c104d
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-04-26 23:22:05 +0200

    Create initial callbacks

--- lib/hive/vehicle_worker.ex
+++ lib/hive/vehicle_worker.ex
@@ -1,6 +1,6 @@
 defmodule Hive.VehicleWorker do
   use GenServer, restart: :transient
-  alias Hive.Vehicle
+  alias Hive.{GeoPosition, Vehicle}
 
   @mod __MODULE__
 
@@ -9,10 +9,19 @@ defmodule Hive.VehicleWorker do
     GenServer.start_link(@mod, vehicle, name: proc_name(vehicle))
   end
 
+  def update(:position, %Vehicle{} = vehicle, %GeoPosition{} = position) do
+    GenServer.cast(proc_name(vehicle), {:update_position, position})
+  end
+
   # Server
   @impl true
   def init(%Vehicle{} = vehicle) do
     {:ok, vehicle}
+  end
+
+  @impl true
+  def handle_cast({:update_position, %GeoPosition{} = position}, %Vehicle{} = vehicle) do
+    {:noreply, %{vehicle | lastKnownPosition: position}}
   end
 
   defp proc_name(%Vehicle{id: id}) do
