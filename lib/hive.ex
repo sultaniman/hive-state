0a07b93e87d78c122e5ea8b114d4e0df22ec6618
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-03 22:51:50 +0200

    Refactor public api

--- lib/hive.ex
+++ lib/hive.ex
@@ -1,56 +1,36 @@
 defmodule Hive do
   @moduledoc false
-  alias Hive.{
-    GeoPosition,
-    Vehicle,
-    VehicleSupervisor,
-    VehicleWorker
-  }
+  use Hive.Base
 
   @doc """
   Infleet `Vehicle` only by it's id.
   Will return `{:error, {:already_started, pid}}`.
   """
-  def infleet(vehicle_id) when is_binary(vehicle_id) do
-    infleet(%Vehicle{id: vehicle_id})
-  end
-
-  @doc """
-  Infleet `%Vehicle{}` and supervise in case if we need to
-  keep more information about `%Vehicle{}`.
-  Will return `{:error, {:already_started, pid}}`.
-  """
-  def infleet(%Vehicle{} = vehicle) do
-    VehicleSupervisor.infleet(vehicle)
-  end
+  defdelegate infleet(vehicle_id), to: VehicleSupervisor
 
   @doc """
   Defleet `Vehicle` only by it's id if there is no process
   then `{:error, :not_found}` returned.
   """
-  def defleet(vehicle_id) when is_binary(vehicle_id) do
-    defleet(%Vehicle{id: vehicle_id})
+  defdelegate defleet(vehicle_id), to: VehicleSupervisor
+
+  @doc """
+  Update position by `vehicle_id`
+  """
+  def update_position(vehicle_id, %GeoPosition{} = position) do
+    VehicleWorker.update(:position, %Vehicle{id: vehicle_id}, position)
   end
 
   @doc """
-  Defleet vehicle, will stop the process and in case
-  if there is no process then `{:error, :not_found}` returned.
+  Check if vehicle is known and supervised
   """
-  def defleet(%Vehicle{} = vehicle) do
-    VehicleSupervisor.defleet(vehicle)
-  end
-
-  def update_position(%Vehicle{} = vehicle, %GeoPosition{} = position) do
-    VehicleWorker.update(:position, vehicle, position)
-  end
-
-  def vehicle_exists?(%Vehicle{} = vehicle) do
-    vehicle
+  def has_member?(vehicle_id) do
+    %Vehicle{id: vehicle_id}
     |> VehicleWorker.make_name()
-    |> VehicleSupervisor.exists?()
+    |> VehicleSupervisor.member?()
   end
 
-  def h3_index(%Vehicle{} = vehicle, resolution) do
-    VehicleWorker.h3(:index, vehicle, resolution)
+  def h3_index(vehicle_id, resolution) do
+    VehicleWorker.h3(:index, %Vehicle{id: vehicle_id}, resolution)
   end
 end
