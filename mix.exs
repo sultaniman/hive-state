51c072074c96d3cfd1f74c2369526c013c5932b0
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-04 23:23:56 +0200

    Add and configure ex_doc

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule Hive.MixProject do
   @vsn "0.1.0"
   @deps [
     {:h3, "~> 3.6"},
+    {:ex_doc, "~> 0.21", only: :dev, runtime: false},
     {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
   ]
 
@@ -13,7 +14,16 @@ defmodule Hive.MixProject do
       version: @vsn,
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: @deps
+      deps: @deps,
+
+      # Docs
+      name: "Hive state",
+      source_url: "https://github.com/hive-fleet/hive-state",
+      homepage_url: "https://github.com/hive-fleet",
+      docs: [
+        main: "Hive",
+        extras: ["README.md"]
+      ]
     ]
   end
 
