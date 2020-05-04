8ec48fbe11f9512996a43fcee3d51a2c7dedb74f
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-04 23:41:06 +0200

    Add more test to vehicle supervisor

---
+++ test/vehicle_supervisor_test.exs
@@ -0,0 +1,36 @@
+defmodule Hive.VehicleSupervisorTest do
+  use ExUnit.Case
+  alias Hive.{
+    Vehicle,
+    VehicleSupervisor
+  }
+
+  alias Hive.Vehicle.Helpers
+
+  describe "vehicle supervisor :: 🚔 " do
+    test "can infleet vehicle" do
+      vehicle = %Vehicle{id: "new-vehicle"}
+      assert {:ok, pid} = VehicleSupervisor.infleet(vehicle)
+    end
+
+    test "can defleet vehicle" do
+      vehicle = %Vehicle{id: "defleet-vehicle"}
+      assert {:ok, infleet_pid} = VehicleSupervisor.infleet(vehicle)
+      assert {:ok, defleet_pid} = VehicleSupervisor.defleet(vehicle)
+      assert infleet_pid == defleet_pid
+    end
+
+    test "can check fleet membership" do
+      vehicle = %Vehicle{id: "check-vehicle"}
+      proc_name = Helpers.make_name(vehicle)
+      assert {:ok, _pid} = VehicleSupervisor.infleet(vehicle)
+      assert VehicleSupervisor.member?(proc_name)
+    end
+
+    test "can get vehicle by id" do
+      vehicle = %Vehicle{id: "get-vehicle"}
+      assert {:ok, _pid} = VehicleSupervisor.infleet(vehicle)
+      assert vehicle = VehicleSupervisor.get_vehicle(vehicle.id)
+    end
+  end
+end
