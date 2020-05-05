5a69e710cb57a9236e7ee6b7f2989fa1c5bbad0e
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-05 21:57:15 +0200

    Add inch, excoveralls and prepare to publish

--- mix.exs
+++ mix.exs
@@ -2,11 +2,23 @@ defmodule Hive.MixProject do
   use Mix.Project
 
   @vsn "0.1.0"
+
   @deps [
     {:h3, "~> 3.6"},
     {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+    {:excoveralls, "~> 0.12", only: :test},
+    {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test]},
     {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
   ]
+
+  @package [
+    name: "hive",
+    files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+    licenses: ["Apache 2.0"],
+    links: %{"GitHub" => "https://github.com/hive-fleet/hive-state"}
+  ]
+
+  @description "In-memory fleet state management"
 
   def project do
     [
@@ -15,7 +27,15 @@ defmodule Hive.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: @deps,
-
+      description: @description,
+      package: @package,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       # Docs
       name: "Hive state",
       source_url: "https://github.com/hive-fleet/hive-state",
