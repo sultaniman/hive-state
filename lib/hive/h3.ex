9ee0183f9a2a8149e552050e0cc81289f8909823
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-06 22:57:15 +0200

    Update todos and prepare Hive.H3 module

---
+++ lib/hive/h3.ex
@@ -0,0 +1,10 @@
+defmodule Hive.H3 do
+  @moduledoc """
+  Interfaces with Erlang H3 and provides abstraction
+  layer to work with vehicles or `%GeoPosition{}`.
+
+  ## Usage
+
+    iex> Hive.H3.num_hexagons(resolution)
+  """
+end
