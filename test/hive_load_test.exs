505abe25cd7ef00cd9ee3d018e01b8cdc4e82945
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-06 21:50:43 +0200

    Add basic load test

---
+++ test/hive_load_test.exs
@@ -0,0 +1,40 @@
+defmodule Hive.LoadTest do
+  @moduledoc false
+  use ExUnit.Case
+  use Hive.Base
+
+  describe "hive load :: 🌈 " do
+    test "infleet 100 vehicles" do
+      Enum.map(1..100, &Hive.infleet("id-100-#{&1}"))
+
+      count =
+        1..100
+        |> Enum.filter(&Hive.has_member?("id-100-#{&1}"))
+        |> length()
+
+      assert count == 100
+    end
+
+    test "infleet 1_000 vehicles" do
+      Enum.map(1..1_000, &Hive.infleet("id-1000-#{&1}"))
+
+      count =
+        1..1_000
+        |> Enum.filter(&Hive.has_member?("id-1000-#{&1}"))
+        |> length()
+
+      assert count == 1_000
+    end
+
+    test "infleet 10_000 vehicles" do
+      Enum.map(1..10_000, &Hive.infleet("id-1000-#{&1}"))
+
+      count =
+        1..10_000
+        |> Enum.filter(&Hive.has_member?("id-1000-#{&1}"))
+        |> length()
+
+      assert count == 10_000
+    end
+  end
+end
