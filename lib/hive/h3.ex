4f5fcdd00c9b6c72d3f066815f53359c5cbc00f0
Sultan Iman <sultan.imanhodjaev@gmail.com>
2020-05-07 23:02:24 +0200

    Create separate Hive.H3 module

--- lib/hive/h3.ex
+++ lib/hive/h3.ex
@@ -7,4 +7,20 @@ defmodule Hive.H3 do
 
     iex> Hive.H3.num_hexagons(resolution)
   """
+  use Hive.Base
+
+  @doc """
+  Indexes the location at the specified resolution
+  """
+  def index(%GeoPosition{latitude: lat, longitude: lon}, resolution) do
+    :h3.from_geo({lat, lon}, resolution)
+  end
+
+  @doc """
+  Indexes the location for given `vehicle_id` at the specified resolution
+  """
+  def index(vehicle_id, resolution) do
+    position = VehicleWorker.get_position(%Vehicle{id: vehicle_id})
+    :h3.from_geo({position.latitude, position.longitude}, resolution)
+  end
 end
